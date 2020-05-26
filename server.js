0d203118fde5a3ef2cd7731aad79bed624333d53
rzounlome <rzounlome@harmonyhit.com>
2020-05-26 13:20:56 -0400

    added objects to db connect to avoid deprication warnings

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 
+const connectDB = require("./config/db");
+
+//Connect Database
+connectDB();
+
 const app = express();
 
 app.get("/", (req, res) => res.send("API Running"));
